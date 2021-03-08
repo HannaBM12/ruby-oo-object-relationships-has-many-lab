44e7bb8f9a03c0842e2985565040be6a4defe584
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-08 17:02:10 -0500

    commit tree 6dfc23a43c11e77e31eff62f3bc04e8e018b13be

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,37 +1,66 @@
-require 'pry'
 
 class Artist
-
-    attr_accessor :name
-
-    @@all_artists = []
-
+        attr_accessor :name
     def initialize(name)
         @name = name
-        @songs = []
-        @@all_artists << self
     end
-
-   def add_song(song)
-        @songs << song
-        song.artist = self
-   end
-
 
     def songs
         Song.all.select{|song| song.artist == self}
     end
 
-    def add_song_by_name (name)
+    def add_song(song)
+        song.artist = self
+    end
+
+    def add_song_by_name(name)
         song = Song.new(name)
         song.artist = self
     end
 
     def self.song_count
-        total = 0
-        @@all_artists.count{|artist| total += artist.songs.length}
-        total
-
+        Song.all.map{|song| song.artist == self}.count
     end
-
 end
+
+
+
+
+
+# require 'pry'
+
+# class Artist
+
+#     attr_accessor :name
+
+#     @@all_artists = []
+
+#     def initialize(name)
+#         @name = name
+#         @songs = []
+#         @@all_artists << self
+#     end
+
+#    def add_song(song)
+#         @songs << song
+#         song.artist = self
+#    end
+
+
+#     def songs
+#         Song.all.select{|song| song.artist == self}
+#     end
+
+#     def add_song_by_name (name)
+#         song = Song.new(name)
+#         song.artist = self
+#     end
+
+#     def self.song_count
+#         total = 0
+#         @@all_artists.count{|artist| total += artist.songs.length}
+#         total
+
+#     end
+
+# end
--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,11 @@
-require 'pry'
 
 class Song
     attr_accessor :artist, :name
-
     @@all = []
-
     def initialize(name)
         @name = name
         @@all << self
     end
-
 
     def self.all
         @@all
@@ -17,10 +13,42 @@ class Song
 
     def artist_name
         if ! self.artist
-            return nil
+            nil
         else
-            return self.artist.name
+            self.artist.name
         end
     end
 
 end
+
+
+
+
+
+
+# require 'pry'
+
+# class Song
+#     attr_accessor :artist, :name
+
+#     @@all = []
+
+#     def initialize(name)
+#         @name = name
+#         @@all << self
+#     end
+
+
+#     def self.all
+#         @@all
+#     end
+
+#     def artist_name
+#         if ! self.artist
+#             return nil
+#         else
+#             return self.artist.name
+#         end
+#     end
+
+# end
